01a0a6f0dde6718f40272dd309a05f2510c462d6
Gurmapreet Singh <heyim.gurman@gmail.com>
2026-03-07 18:15:04 +0530

    Added the map data in the hospital page

---
+++ js/script.js
@@ -0,0 +1,72 @@
+// create map
+
+var map = L.map('map').setView([20.5937,78.9629],5);
+
+// load OpenStreetMap tiles
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+attribution:'© OpenStreetMap'
+}).addTo(map);
+
+
+// get user location
+
+navigator.geolocation.getCurrentPosition(function(position){
+
+let lat = position.coords.latitude;
+let lon = position.coords.longitude;
+
+map.setView([lat,lon],14);
+
+
+// user marker
+
+L.marker([lat,lon])
+.addTo(map)
+.bindPopup("You are here")
+.openPopup();
+
+
+// find hospitals
+
+fetchHospitals(lat,lon);
+
+});
+
+
+// function to fetch hospitals
+
+function fetchHospitals(lat,lon){
+
+let radius = 20000;
+
+let query = `
+[out:json];
+(
+node["amenity"="hospital"](around:${radius},${lat},${lon});
+);
+out;
+`;
+
+let url = "https://overpass-api.de/api/interpreter?data=" + encodeURIComponent(query);
+
+fetch(url)
+.then(res => res.json())
+.then(data => {
+
+data.elements.forEach(hospital => {
+
+let hLat = hospital.lat;
+let hLon = hospital.lon;
+
+let name = hospital.tags.name || "Hospital";
+
+L.marker([hLat,hLon])
+.addTo(map)
+.bindPopup("🏥 " + name);
+
+});
+
+});
+
+}
